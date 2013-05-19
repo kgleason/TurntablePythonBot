ec6bf0b1d0aba872f1f20681a1c2328aca4aa1aa
Kirk gleason <kgleason@bloommg.com>
2013-05-19 13:56:10 -0400

    Added in a SQL call to get popular items

--- BotDB.py
+++ BotDB.py
@@ -14,26 +14,8 @@ def checkDatabaseVersion(dbFile):
 
 
 	except sql.Error, e:
-		# If we don't have a Version table, then we need to create the entire schema
-		cur.executescript("""
-		DROP TABLE IF EXISTS BotDbVersion;
-		DROP TABLE IF EXISTS SongHistory;
-		DROP TABLE IF EXISTS UserHistory;
-		DROP TABLE IF EXISTS VotingHistory;
-		DROP TABLE IF EXISTS SnagHistory;
-		DROP TABLE IF EXISTS BotOperators;
-		DROP TABLE IF EXISTS DjQueue;
-		CREATE TABLE BotDbVersion (versionId INTEGER PRIMARY KEY, version INTEGER);
-		CREATE TABLE SongHistory (songHistoryId INTEGER PRIMARY KEY, songID TEXT, songPlayDateTime TEXT, userID TEXT, songLength INT, songArtist TEXT, songName TEXT, songAlbum TEXT);
-		CREATE TABLE UserHistory (UserHistoryId INTEGER PRIMARY KEY, userID TEXT, seenDateTime TEXT, userName TEXT, action TEXT);
-		CREATE TABLE VotingHistory (VotingHistoryId INTEGER PRIMARY KEY, voteType TEXT, userID TEXT, songID TEXT, voteDateTime TEXT, djID TEXT);
-		CREATE TABLE SnagHistory (SnagHistoryID INTEGER PRIMARY KEY, userID TEXT, songID TEXT, snagDateTime TEXT);
-		CREATE TABLE ThemeHistory (ThemeHistoryID INTEGER PRIMARY KEY, themeText TEXT, userID TEXT, themeSetDateTime TEXT);
-		CREATE TABLE BotOperators (BotOperatorID INTEGER PRIMARY KEY, userID TEXT);
-		CREATE TABLE DjQueue (DjQueueID INTEGER PRIMARY KEY, userID TEXT);
-		""")
-
-		conn.commit()
+		# If we don't have a Version table, then we need will recreate the entire schema
+		# Set the dbVersion back to the beginning
 		dbVersion = 0
 	finally:
 		upgradeDatabase(conn,dbVersion)
@@ -55,21 +37,41 @@ def upgradeDatabase(con,ver):
 
 	#Initial state
 	if ver == 0:
-		print 'Upgrading database from version {}'.format(ver)
-		ver += 1
+		print 'Upgrading database to version {}'.format(ver)
 		with con:
 			try:
 				cur = con.cursor()
-				cur.execute("INSERT INTO BotDbVersion(version) VALUES (?)",str(ver))
+				cur.executescript("""
+					DROP TABLE IF EXISTS BotDbVersion;
+					DROP TABLE IF EXISTS SongHistory;
+					DROP TABLE IF EXISTS UserHistory;
+					DROP TABLE IF EXISTS VotingHistory;
+					DROP TABLE IF EXISTS SnagHistory;
+					DROP TABLE IF EXISTS BotOperators;
+					DROP TABLE IF EXISTS DjQueue;
+					CREATE TABLE BotDbVersion (versionId INTEGER PRIMARY KEY, version INTEGER);
+					CREATE TABLE SongHistory (songHistoryId INTEGER PRIMARY KEY, songID TEXT, songPlayDateTime TEXT, userID TEXT, songLength INT, songArtist TEXT, songName TEXT, songAlbum TEXT);
+					CREATE TABLE UserHistory (UserHistoryId INTEGER PRIMARY KEY, userID TEXT, seenDateTime TEXT, userName TEXT, action TEXT);
+					CREATE TABLE VotingHistory (VotingHistoryId INTEGER PRIMARY KEY, voteType TEXT, userID TEXT, songID TEXT, voteDateTime TEXT, djID TEXT);
+					CREATE TABLE SnagHistory (SnagHistoryID INTEGER PRIMARY KEY, userID TEXT, songID TEXT, snagDateTime TEXT);
+					CREATE TABLE ThemeHistory (ThemeHistoryID INTEGER PRIMARY KEY, themeText TEXT, userID TEXT, themeSetDateTime TEXT);
+					CREATE TABLE BotOperators (BotOperatorID INTEGER PRIMARY KEY, userID TEXT);
+					CREATE TABLE DjQueue (DjQueueID INTEGER PRIMARY KEY, userID TEXT);
+					INSERT INTO BotDbVersion (version) VALUES (0);
+				""")
 				con.commit()
 			except sql.Error, e:
 				print 'Caught a SQLite Exception: {}'.format(e)
+		ver += 1
 
 	if ver == 1:
-		print 'Upgrading database from version {}'.format(ver)
-		ver += 1
+		print 'Upgrading database to version {}'.format(ver)
+		with con:
+			cur = con.cursor()
+			cur.execute("UPDATE BotDBVersion SET version = ? WHERE version = ?",(ver,ver-1))
 		# This is the most recent version, nothing to do here, for now
 		print 'Database is up to date.'
+		ver += 1
 
 def addSongHistory(con, sid, uid, length, artist, name, album):
 	with con:
@@ -115,12 +117,11 @@ def saveDjQueue(con, djQueue):
 		cur.executemany("INSERT INTO DjQueue (userID) VALUES (?)",djQueue)
 		cur.commit()
 
-def getBusiestDJs(con, cnt):
+def getMostSongData(con, cnt, songItem):
 	with con:
 		cur = con.cursor()
-		cur.execute("SELECT COUNT(*) AS SongsPlayed, userID FROM SongHistory GROUP BY userID ORDER BY 1 DESC LIMIT ?", (str(cnt)))
+		sql="SELECT COUNT(*), {} FROM SongHistory GROUP BY {} ORDER BY 1 DESC LIMIT {}".format(songItem,songItem,str(cnt))
+		cur.execute(sql)
 		rows = cur.fetchall()
-		print 'SQL returned the following results: {}'.format(rows)
-	print 'SQL returned the following results: {}'.format(rows)
 	return rows
 
